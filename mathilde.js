3a1fcc2109dca1060d0d2bc1d3115fbecebe84a3
Mathilde-Christensen <mathildemc27@gmail.com>
2025-02-10 16:02:15 +0100

    opdagteringe af søgeknap fukntion

--- mathilde.js
+++ mathilde.js
@@ -1,5 +1,16 @@
 function soeFeltIndput() { //funktionen kører, når knappen "Søg"bliver trykket
     let indputFelt = document.getElementById("indputFelt"); //Gemmer elementet i en variabel, gør det muligt at manipulere i elementer fra HTML (dom)
     let lukKnap = document.getElementById("lukKnap"); //Gemmer elementet i en variabel, gør det muligt at manipulere i elementer fra HTML (dom)
-    
+    let soegefeltButton = document.getElementById("soegefeltButton");
+
+    if (indputFelt.style.display === "none") {
+        indputFelt.style.display = "block";  // Gør søgefeltet synligt
+        lukKnap.style.display = "block";  // Gør X-knappen synlig
+        soegefeltButton.style.display = "none";
+    } else {
+        indputFelt.style.display = "none";  // Skjul feltet
+        lukKnap.style.display = "none";  // Skjul knappen
+        soegefeltButton.style.display = "block";
+        indputFelt.value = ""; //indputfeltet bliver tomt når man lukker søgefeltet ned iegn
+    }
 }
