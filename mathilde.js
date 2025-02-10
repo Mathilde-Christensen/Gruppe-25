99948e26e18715703e2d66c5e7dcf4ed3060e009
Mathilde-Christensen <mathildemc27@gmail.com>
2025-02-10 14:08:38 +0100

    display non

--- mathilde.js
+++ mathilde.js
@@ -1,4 +1,5 @@
-function soeFeltIndput() {
-    let indputFelt = document.getElementById("sergeFeltIndputFelt");
+function soeFeltIndput() { //funktionen kører, når knappen "Søg"bliver trykket
+    let indputFelt = document.getElementById("indputFelt"); //Gemmer elementet i en variabel, gør det muligt at manipulere i elementer fra HTML (dom)
+    let lukKnap = document.getElementById("lukKnap"); //Gemmer elementet i en variabel, gør det muligt at manipulere i elementer fra HTML (dom)
     
 }
