541d461c331838edc63c7acecee2c7db8027043e
k1rja <kirjabanke1999@gmail.com>
2025-02-11 12:12:31 +0100

    if-else nextButton tilføjet

--- kirja.js
+++ kirja.js
@@ -11,3 +11,16 @@ for (let i = 0; i < totalImages; i += imagesPerSlide){ //For-loop til at beregne
     maxIndex++;
 }
 maxIndex--; //Trækker 1 fra, da vi starter på index 0
+
+const imageWidth = images[0].clientWidth + 10; //Billedets bredde plus margin
+const scrollAmount = imageWidth * imagesPerSlide //Hvor langt der skal scrolles
+
+nextButton.addEventListener("click", function(){
+    if(index < maxIndex){
+        index++;
+    } else{
+        index = 0; //Så looper vi tilbage til første billederække
+    }
+    track.computedStyleMap.transform
+}
+)
