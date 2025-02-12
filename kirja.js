83dcbcf06bd61353028cde5b77c49f46a5adc109
k1rja <kirjabanke1999@gmail.com>
2025-02-12 13:14:52 +0100

    rettet i css, html og js billedeGalleri

--- kirja.js
+++ kirja.js
@@ -1,20 +1,17 @@
 const track = document.querySelector(".gallery-track");
-const images = document.querySelectorAll(".gallery-track img");
+const images = document.querySelectorAll(".gallery-track .gallery-item");
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
 
 let index = 0;
 const imagesPerSlide = 7; //Antal synlige billede ad gangen
 const totalImages = images.length;
+const imageWidth = images[0].offsetWidth + 20; //Billedets bredde plus margin
+const scrollAmount = imageWidth * imagesPerSlide //Hvor langt der skal scrolles
 const maxIndex = Math.ceil(totalImages / imagesPerSlide)-1;
 
 function updateScroll(){
     track.style.transform = `translateX(-${index * scrollAmount}px)`;
-}
-const imageWidth = images[0].offsetWidth + 10; //Billedets bredde plus margin
-const scrollAmount = imageWidth * imagesPerSlide //Hvor langt der skal scrolles
-function updateScroll(){
-  track.style.transform = `translateX(-${index * scrollAmount}px)`;
 }
 prevButton.addEventListener("click", function(){
     if (index > 0){
